b62cb2cf465e5e4f66ae022fb4282a614bbd1697
邓雪莹 <425836232@qq.com>
2017-01-01 14:42:53 +0800

    团购详情过期时间处理

--- MeiTuanHD/MeiTuanHD/Classes/Detail/View/MTDetailBottomView.m
+++ MeiTuanHD/MeiTuanHD/Classes/Detail/View/MTDetailBottomView.m
@@ -39,9 +39,45 @@
 -(void)setDealModel:(MTDealModel *)dealModel{
     _dealModel = dealModel;
     //赋值
-    self.btnA.selected = dealModel.restrictions.is_reservation_required;
-    self.btnC.selected = dealModel.restrictions.is_refundable;
+    // A 随时退款
+    self.btnA.selected = dealModel.restrictions.is_refundable;
+    // C 过期退款
+    self.btnC.selected = dealModel.restrictions.is_reservation_required;
+    // D 已售数量
     [self.btnD setTitle:[NSString stringWithFormat:@"已售 %zd",dealModel.purchase_count] forState:UIControlStateNormal];
+    // B 过期时间
+    [self.btnB setTitle:[self dealPurchaseDeadline:dealModel.purchase_deadline] forState:UIControlStateNormal];
+    
+}
+
+//计算截止购买时间
+- (NSString *)dealPurchaseDeadline:(NSString *)deadline{
+    //获取当前时间
+    NSDate * currentDate = [NSDate date];
+    //时间格式化
+    NSDateFormatter * df = [NSDateFormatter new];
+    //设置格式
+    df.dateFormat = @"yyyy-MM-dd";
+    //需要把团购过期字符串转日期
+    NSDate * dealDate = [df dateFromString:deadline];
+    //时间比较组件
+    NSCalendarUnit unit = NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute;
+    
+    //时间比较
+    NSDateComponents * cmp = [[NSCalendar currentCalendar] components:unit fromDate:currentDate toDate:dealDate options:0];
+    
+    NSLog(@"%ld天%ld小时%ld分钟",cmp.day,cmp.hour,cmp.minute);
+    
+    //如果天数>365 -> 一年之内都不会过期
+    if (cmp.day > 365) {
+        return @"一年之内都不会过期";
+    }else if(cmp.day + cmp.hour + cmp.minute <= 0){
+        //如果day天 小时 分钟均为0 , 就显示已经过期
+        return @"已过期";
+    }else{
+        return [NSString stringWithFormat:@"%ld天%ld小时%ld分钟",cmp.day,cmp.hour,cmp.minute];
+    }
+    
 }
 
 
@@ -80,6 +116,7 @@
 //创建和添加按钮的公共方法
 - (UIButton *)addChildButtons:(NSString *)title{
     UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.userInteractionEnabled = false;
     [btn setImage:[UIImage imageNamed:@"icon_order_unrefundable"] forState:UIControlStateNormal];
     [btn setImage:[UIImage imageNamed:@"icon_order_refundable"] forState:UIControlStateSelected];
     [btn setTitle:title forState:UIControlStateNormal];
